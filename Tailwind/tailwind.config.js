87eab136b1174da17b392065d1bef6d99fa2a729
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-05-09 01:37:55 -0500

    Color y margenes

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#3AB0FF",
+          default: "#007BFF",
+          dark: "#0056B3",
+        },
+      },
+    },
   },
   plugins: [],
 };
